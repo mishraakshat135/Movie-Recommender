3f65600df4858728bdaaef62530c88a51f28a3bd
mishraakshat135 <mishraakshat169@gmail.com>
2026-06-24 18:50:18 +0530

    render link added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-API_BASE =  "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommender-rp3g.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
